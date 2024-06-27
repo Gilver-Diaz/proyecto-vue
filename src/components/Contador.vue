aa76e51263dea91e8eb7bd031f87ac5e465b953f
Gilver-Diaz <gilveryadrieldiazfigueroa@gmail.com>
2024-06-27 14:50:27 -0500

    Cree componente de Contador de

---
+++ src/components/Contador.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template> 
+<h1>Hola Mundo </h1>
+<p>{{ contador }}</p> 
+ <button @click="incrementar">Incrementar</button>
+ <button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+ import { ref } from 'vue';
+  const contador  = ref (0);
+  const incrementar = () => {
+    contador.value++; 
+  }
+  const decrementar = () => {
+    contador.value--; 
+  }
+</script>
+
+<style scoped>
+ p {
+  color: rgb(85, 42, 165);
+ }
+</style>
